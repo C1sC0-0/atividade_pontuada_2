760edb6f2f62df231b6e13a746d0aff33698e108
C1sC0-0 <guilhermefrancisco0711@gmail.com>
2025-03-31 16:53:19 -0300

    Update menu.py

--- menu_restaurante.py/menu.py
+++ menu_restaurante.py/menu.py
@@ -8,30 +8,30 @@ escolhidos = ""
 while True:
     print( """
           Código \t Prato \t\t Valor
-          1 \t Picanha \t\t R$25,00
-          2 \t Lasanha \t\t R$20,00
-          3 \t Strogonoff \t\t R$18,00
+          1 \t Picanha \t\t R$30,00
+          2 \t Lasanha \t\t R$25,00
+          3 \t Strogonoff \t\t R$20,00
           4 \t Bife Acebolado \t R$15,00
           5 \t Pão com ovo \t\t R$5,00
           6 \t Misto \t\t\t R$7,00              
-          7 \t Milkshake \t\t R$20,00     
+          7 \t Bobó de camarão \t\t R$20,00     
 """)
     opcao = int (input("Digite o numero da opção desejada: "))
     
     match opcao:
         case 1:
-            prato = "1. Picanha "
-            preco = 25 
+            prato = "1- Picanha "
+            preco = 30
             pr += 1
             escolhidos += prato
         case 2:
-            prato = "2. Lasanha "
-            preco = 20
+            prato = "2- Lasanha "
+            preco = 25
             pr += 1
             escolhidos += prato
         case 3:
-            prato = "3. Strogonoff "
-            preco = 18
+            prato = "3- Strogonoff "
+            preco = 20
             pr += 1
             escolhidos += prato
         case 4:
@@ -50,7 +50,7 @@ while True:
             pr += 1
             escolhidos += prato
         case 7:
-            prato = "7 . Milkshake " 
+            prato = "7 . Bobo de camarão" 
             preco = 20
             pr += 1
             escolhidos += prato
@@ -78,8 +78,8 @@ match forma_de_pagamento:
     case 1:
         valort = soma * 0.9
         print("Desconto de 10%.")
-        print(f"Total a pagar: R${valort}")
+        print(f"Total a pagar: R${valortotal}")
     case 2:
         valort = soma * 1.1
         print(f"Acréscimo de 10%.")
-        print(f"Total a pagar: R${valort}")
+        print(f"Total a pagar: R${valortotal}")
